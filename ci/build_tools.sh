37ecbc8ea02d18e02c9df1a3ff50b18946cecc11
Hao Guan <10684225+hguandl@users.noreply.github.com>
2021-10-15 04:01:46 +0800

    添加编译工具 Action

---
+++ ci/build_tools.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+apt-get update
+apt-get install -y cmake ninja-build
+
+git clone https://github.com/3DSGuy/Project_CTR.git
+pushd Project_CTR
+pushd ctrtool
+make
+popd
+pushd makerom
+make
+popd
+popd
+
+git clone https://github.com/dnasdw/3dstool.git
+pushd 3dstool
+mkdir build
+pushd build
+cmake -GNinja ..
+ninja
+popd
+popd
+
+mkdir bin
+cp ./Project_CTR/ctrtool/ctrtool ./bin/
+cp ./Project_CTR/makerom/makerom ./bin/
+cp ./3dstool/bin/Release/3dstool ./bin/
+
+tar zcf rom_tools.tar.gz ./bin
