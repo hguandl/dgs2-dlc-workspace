ebc1f741d0e4c4ebb28e94a34fc67a9456846600
Hao Guan <10684225+hguandl@users.noreply.github.com>
2021-10-16 13:04:03 +0800

    使用ACG人名字库

--- ci/01-build_fonts.sh
+++ ci/01-build_fonts.sh
@@ -12,7 +12,6 @@ mkdir -p $FONT_RES_DIR
 $GFD_EXE dump -o $FONT_RES_DIR $ROMFS_DIR/font/font00_jpn.gfd
 $GFD_EXE dump -o $FONT_RES_DIR $ROMFS_DIR/font/font01_jpn.gfd
 $GFD_EXE dump -o $FONT_RES_DIR $ROMFS_DIR/font/font02_jpn.gfd
-$GFD_EXE dump -o $FONT_RES_DIR $ROMFS_DIR/font/font03_jpn.gfd
 
 $FONT_DB_EXE count -d ./msg_dump -o $FONT_RES_DIR/msg_text.json
 $FONT_DB_EXE count -d ./script_dump -o $FONT_RES_DIR/script_text.json
@@ -23,9 +22,9 @@ $FONT_DB_EXE merge -o $FONT_RES_DIR/font01_jpn_list.json \
     ./font_acg/font01_acg.json $FONT_RES_DIR/msg_text.json
 $FONT_DB_EXE merge -o $FONT_RES_DIR/font02_jpn_list.json \
     ./font_acg/font02_acg.json $FONT_RES_DIR/msg_text.json
-cp font_acg/font03_acg.json $FONT_RES_DIR/font03_jpn_list.json
 
 $GFD_EXE generate -i $FONT_RES_DIR -f ./external/hanyi-65s.ttf -n 00 -o $ROMFS_DIR/font
 $GFD_EXE generate -i $FONT_RES_DIR -f ./external/dayasong.ttf -n 01 -o $ROMFS_DIR/font
 $GFD_EXE generate -i $FONT_RES_DIR -f ./external/dayasong.ttf -n 02 -o $ROMFS_DIR/font
-$GFD_EXE generate -i $FONT_RES_DIR -f ./external/SourceHanSans-Bold.otf -n 03 -o $ROMFS_DIR/font
+cp font_acg/font03_jpn_00_AM_NOMIP.tex $ROMFS_DIR/font/
+cp font_acg/font03_jpn.gfd $ROMFS_DIR/font/
